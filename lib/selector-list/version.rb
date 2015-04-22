28af01a3498b54233a17cb779e6f447c9f4aa3dc
Federico Aloi <federico.aloi@gmail.com>
2015-04-22 19:24:25 -0300

    Initial commit

---
+++ lib/selector-list/version.rb
@@ -0,0 +1,3 @@
+module SelectorList
+  VERSION = "0.0.1"
+end
---
+++ spec/a_descubrir_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../lib/selector_list'
+
+RSpec.describe "Dummy" do
+  it "test" do
+    expect(true).to eq(true)
+  end
+end
